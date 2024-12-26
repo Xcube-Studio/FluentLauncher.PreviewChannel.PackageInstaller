d716cf96c20160eb7e47c134f87ab1d65fbb124b
natsurainko <a-275@qq.com>
2024-12-26 18:17:33 +0800

    Create ReleaseScripts.cs

---
+++ FluentLauncher.PreviewChannel.PackageInstaller/Scripts/ReleaseScripts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentLauncher.PreviewChannel.PackageInstaller.Scripts;
+
+//{
+//    "commit": "7c9061e",
+//    "build": 1,
+//    "releaseTime": "",
+//    "previousStableVersion": "2.3.2.0",
+//    "hashes": {
+//        "updatePackage-x64.zip": "",
+//        "updatePackage-arm64.zip": ""
+//    }
+//}
+
+public static class ReleaseScripts
+{
+    public static async Task GenerateReleaseJson(string commit, string[] packageFiles)
+    {
+
+    }
+}
